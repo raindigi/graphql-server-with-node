e2492b272d721cb7f4805f59c6cfd0ad744bd149
saurabharch <saurabhkashyap0001@gmail.com>
2019-04-13 05:07:57 +0530

    initial commit

---
+++ src/resolvers.js
@@ -0,0 +1,22 @@
+import {Cat} from "./models/Cat";
+
+export const resolvers = {
+    Query: {
+        hello: () => "hi",
+        cats: () => Cat.find()
+    },
+    Mutation: {
+        createCat: async (_, {
+            name
+        }) => {
+            const kitty = new Cat({
+                name
+            });
+            await kitty.save();
+            return kitty;
+        }
+    }
+};
+
+// const kitty = new Cat({ name: 'Zildjian' });
+// kitty.save().then(() => console.log('meow'));
